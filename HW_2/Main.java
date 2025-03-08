4de351b63823acf2be1e4db04095381c2ae06f79
undertaker4141 <a8925525a@gmail.com>
2025-03-09 01:28:23 +0800

    hw_2_初始模板

---
+++ HW_2/Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) throws java.io.IOException {
+    java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    String[] input = br.readLine().split(" ");
+    
+    }
+  }
+  
